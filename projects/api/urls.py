f45548e486a14b1743c47d1fcd96b73c98855209
dijodill <dijo.dill@gmail.com>
2026-02-07 13:08:03 +0000

    refactor: remove GeoPackage import endpoint from projects API URLs
    Remove ImportGeoPackageAPIView import and /import/ endpoint from URL patterns

--- projects/api/urls.py
+++ projects/api/urls.py
@@ -4,11 +4,9 @@ from .projects import (
     ProjectDetailAPIView,
     LatestProjectUpdatesAPIView,
 )
-from ..services.imports import ImportGeoPackageAPIView
 
 urlpatterns = [
     path("projects/", ProjectListCreateAPIView.as_view()),
     path("projects/latest/", LatestProjectUpdatesAPIView.as_view()),
     path("projects/<uuid:project_id>/", ProjectDetailAPIView.as_view()),
-    path("projects/<uuid:project_id>/import/", ImportGeoPackageAPIView.as_view(),),
 ]
