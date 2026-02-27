0499bb85701cdddb9c3e69012337bf50d75aedcd
dijodill <dijo.dill@gmail.com>
2026-02-27 10:54:44 +0000

    added new endpoints for engineer activity

--- assignments/api/urls.py
+++ assignments/api/urls.py
@@ -4,6 +4,10 @@ from assignments.api.views import (
     AssignmentJobDetailAPIView,
     AssignmentJobListCreateAPIView,
     AssignmentJobSummaryAPIView,
+    EngineerActivityAPIView,
+    EngineerStatsAPIView,
+    FeatureFieldMeasurementsAPIView,
+    FeatureSubmitAPIView,
     JobAssignmentsListAPIView,
 )
 
@@ -12,4 +16,8 @@ urlpatterns = [
     path("assignments/summary/", AssignmentJobSummaryAPIView.as_view()),
     path("assignments/<uuid:pk>/", AssignmentJobDetailAPIView.as_view()),
     path("assignments/jobs/", JobAssignmentsListAPIView.as_view()),
+    path("engineer/activity/", EngineerActivityAPIView.as_view()),
+    path("engineer/stats/", EngineerStatsAPIView.as_view()),
+    path("features/<uuid:pk>/field-measurements/", FeatureFieldMeasurementsAPIView.as_view()),
+    path("features/submit/", FeatureSubmitAPIView.as_view()),
 ]
