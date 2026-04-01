4e82503c4a4797a6b26e5928ee495dae91ac7872
dijodill <dijo.dill@gmail.com>
2026-04-01 10:38:54 +0100

    Update: Modify database settings for production environment

--- config/settings.py
+++ config/settings.py
@@ -69,27 +69,45 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'HOST': os.getenv('POSTGRES_HOST', 'fiber.crwqq2g60zoe.eu-west-2.rds.amazonaws.com'),
+#         'PORT': os.getenv('POSTGRES_PORT', '5432'),
+#         'NAME': os.getenv('POSTGRES_DB', 'postgres'),
+#         'USER': os.getenv('POSTGRES_USER', 'postgres'),
+#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+#         'CONN_MAX_AGE': 60,
+#     },
+#     'gis': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'postgis.crwqq2g60zoe.eu-west-2.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.getenv('POSTGRES_HOST', 'fiber.crwqq2g60zoe.eu-west-2.rds.amazonaws.com'),
-        'PORT': os.getenv('POSTGRES_PORT', '5432'),
-        'NAME': os.getenv('POSTGRES_DB', 'postgres'),
-        'USER': os.getenv('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
-        'CONN_MAX_AGE': 60,
+        'NAME': 'zeabur_db',
+        'USER': 'zeabur_user',
+        'PASSWORD': 'ybSCV1v2RLdP90xZg73fq456uOhpD8iJ',
+        'HOST': '43.157.58.101',
+        'PORT': '31768',
     },
     'gis': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'postgis.crwqq2g60zoe.eu-west-2.rds.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'fiber_gis',
+        'USER': 'zeabur_user',
+        'PASSWORD': 'ybSCV1v2RLdP90xZg73fq456uOhpD8iJ',
+        'HOST': '43.157.58.101',
+        'PORT': '31768',
     }
 
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
